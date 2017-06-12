7d1f108ef328a0975c9124e54f33ac167df25035
daniel <danieldopp@uky.edu>
2017-06-12 15:03:01 -0600

    Added pickling function to store synonym dict and full object list.

--- NCI_flat_file_parser.py
+++ NCI_flat_file_parser.py
@@ -1,4 +1,6 @@
 import csv
+import pickle
+
 from NCIConcept import NCIConcept
 
 if __name__ == "__main__":
@@ -8,15 +10,20 @@ if __name__ == "__main__":
         "definition", "display_name",
         "concept_status", "semantic_type"
     )
-    comprehensive_concept_dict = {}
+    # comprehensive_concept_dict = {}
     # type_set = set()
+    concept_list = []
     with open(r"/home/daniel/Downloads/Thesaurus.txt") as file:
         reader = csv.DictReader(file, fieldnames=fieldnames, delimiter="\t")
         for row in reader:
             # for item in row["semantic_type"].split("|"):
                 # type_set.add(item)
             concept = NCIConcept(row)
+            concept_list.append(concept)
         # with open("/home/daniel/Downloads/semantic_types.txt", "w") as types:
         #     for concept_type in sorted(type_set):
         #         types.write(concept_type + "\n")
-            comprehensive_concept_dict[concept.concept_name] = concept.synonyms
+            # comprehensive_concept_dict[concept.concept_name] = list(concept.synonyms)
+    with open("/home/daniel/Downloads/NCIFullConcepts.p", "wb") as file:
+        pickle.dump(concept_list, file)
+        # pickle.dump(comprehensive_concept_dict, file)
