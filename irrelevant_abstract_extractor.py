91fc26553af9ff7ac1177db0102113febe0f0753
daniel <danieldopp@uky.edu>
2017-06-19 16:15:17 -0600

    Altered flow of computation to increase efficiency, changed paths to linux paths

--- irrelevant_abstract_extractor.py
+++ irrelevant_abstract_extractor.py
@@ -7,19 +7,19 @@ from BioCObject import BioCObject
 
 if __name__ == "__main__":
     dis_training_ids = abs_extract.get_training_set_pmid(
-        r"C:\Users\Danie\Downloads\TaskData\task1trainingdata\task1trainingdata\DIS_train.qrel")
+        r"/home/daniel/Downloads/task1trainingdata/task1trainingdata/DIS_train.qrel")
+    print(len(dis_training_ids))
     num_abstracts = len(dis_training_ids)
     id_to_abstract = {}
     count = 0
-    abstracts = BioCObject(r"C:\Users\Danie\Downloads\TaskData\abstracts_collection.xml").abstracts()
-    while count < num_abstracts:
-        rand_abstract_id = random.choice(abstracts.keys())
-        if rand_abstract_id in dis_training_ids:
-            pass
-        else:
+    abstracts = BioCObject(r"/home/daniel/Downloads/abstracts_collection.xml").abstracts()
+    temp = list(abstracts.keys())
+    while count != num_abstracts:
+        rand_abstract_id = random.choice(temp)
+        if rand_abstract_id not in dis_training_ids and rand_abstract_id not in id_to_abstract:
             id_to_abstract[rand_abstract_id] = abstracts[rand_abstract_id]
             count += 1
     print(len(id_to_abstract))
-    with open(r"C:\Users\Danie\Downloads\dis_irrelevant_abstracts.pkl", "wb") as file:
+    with open(r"/home/daniel/Downloads/dis_irrelevant_abstracts.pkl", "wb") as file:
         pickle.dump(id_to_abstract, file)
     print("done processing")
