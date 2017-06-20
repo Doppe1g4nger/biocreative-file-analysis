65246a70e126ba9801204d9c94f6e4a93f89ddc9
daniel <danieldopp@uky.edu>
2017-06-20 13:14:19 -0600

    Added getter functions and adjusted class initialization logic.

--- Annotation.py
+++ Annotation.py
@@ -11,10 +11,21 @@ class Annotation:
         self._ontology = kwargs.get("ontology", None)
         self._id = kwargs.get("id", None)
         try:
-            if self._text is None or self._start is None or self._end is None:
-                if len(args) < 3:
-                    raise ValueError("Too few arguments passed, Annotation must have at least text, start, and end")
-            if len(args) == 3:
+            if not args:
+                if self._text is None or self._start is None or self._end is None:
+                    raise ValueError("Too few arguments passed, Annotation must have at least text, start, and end", 0)
+            elif len(args) == 1:
+                if self._start is None or self._end is None:
+                    raise ValueError("Too few arguments passed, Annotation must have at least text, start, and end", 0)
+                else:
+                    self._text = args[0]
+            elif len(args) == 2:
+                if self._end is None:
+                    raise ValueError("Too few arguments passed, Annotation must have at least text, start, and end", 0)
+                else:
+                    self._text = args[0]
+                    self._start = args[1]
+            elif len(args) == 3:
                 self._text = args[0]
                 self._start = args[1]
                 self._end = args[2]
@@ -29,4 +40,33 @@ class Annotation:
                 self._end = args[2]
                 self._ontology = args[3]
                 self._id = args[4]
+            else:
+                self._text = args[0]
+                self._start = args[1]
+                self._end = args[2]
+                self._ontology = args[3]
+                self._id = args[4]
+                raise ValueError("Too many arguments passed, Some parameter were ignored", 1)
+        except ValueError as err:
+            if err.args[1] == 1:
+                print(err.args[0])
+            else:
+                raise
 
+    def get_text(self):
+        return self._text
+
+    def get_start(self):
+        return self._start
+
+    def get_end(self):
+        return self._end
+
+    def get_bounds(self):
+        return self._start, self._end
+
+    def get_id(self):
+        return self._id
+
+    def get_ontology(self):
+        return self._ontology
