ddd1efb1007d52ff803715ff5f49f4f80f03e84a
daniel <danieldopp@uky.edu>
2017-07-12 15:05:28 -0600

    Updated replace path variable function to handle strings with no variable, Made Hashing version of bag of words test script Updated ranking function made scratch function to make dict of kinase to id names Added pathvar replace function to helpers

--- helper_functions.py
+++ helper_functions.py
@@ -1,11 +1,11 @@
 import os.path as path
-from os import listdir
 from email.mime.text import MIMEText
 import smtplib
+import os
 
 
 def get_all_files(fpath):
-    return [path.join(fpath, f) for f in listdir(fpath) if path.isfile(path.join(fpath, f))]
+    return [path.join(fpath, f) for f in os.listdir(fpath) if path.isfile(path.join(fpath, f))]
 
 
 def send_email(sender, password, receivers, subject, message="", server="smtp-mail.outlook.com", protocol=587):
@@ -22,3 +22,16 @@ def send_email(sender, password, receivers, subject, message="", server="smtp-ma
 
 def input_true_or_false(user_input, true_cond={"y"}):
     return True if user_input in true_cond else False
+
+
+def replace_pathvar_with_environ(string):
+    path_var_start = string.find("$")
+    if path_var_start != -1:
+        path_slice = string[path_var_start + 1:string.find("/", path_var_start)]
+    else:
+        return string
+    try:
+        path_replacement = os.environ[path_slice]
+        return replace_pathvar_with_environ(string.replace(path_slice, path_replacement))
+    except:
+        raise
