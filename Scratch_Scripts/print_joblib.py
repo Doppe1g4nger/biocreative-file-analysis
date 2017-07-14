65f5dd3abb646b6e7f8fa39a6645d03b09f41612
daniel <danieldopp@uky.edu>
2017-07-14 13:10:58 -0600

    changed ini

---
+++ Scratch_Scripts/print_joblib.py
@@ -0,0 +1,10 @@
+try:
+    from machine_learning_tests import helper_functions as h
+except ImportError:
+    import helper_functions as h
+from sklearn.externals import joblib
+
+if __name__ == "__main__":
+    for file in h.get_all_files("./"):
+        thing = joblib.load(file)
+        print(thing, type(thing))
