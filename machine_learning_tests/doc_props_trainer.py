dd61b4ad3f7614756d95f9cc6d4988cfd17c7684
daniel <danieldopp@uky.edu>
2017-07-21 13:07:20 -0600

    changed parameters

--- machine_learning_tests/doc_props_trainer.py
+++ machine_learning_tests/doc_props_trainer.py
@@ -35,9 +35,9 @@ if __name__ == "__main__":
         parameters = {
             "probability": [True],
             "C": [0.01, 0.1, 1.0, 10.0, 100.0],
-            "kernel": ["poly", "rbf", "sigmoid"],
-            "degree": [1, 2, 3, 4, 5],
-            # "clf__coef0": [0.0, 0.1, 0.5, 0.7, 1.0],
+            # "kernel": ["poly", "rbf", "sigmoid"],
+            # "degree": [1, 2, 3, 4, 5],
+            "clf__coef0": [0.0, 0.1, 0.5, 0.7, 1.0],
             # "clf__shrinking": [True, False],
             # "clf__class_weight": ["balanced", None]
         }
