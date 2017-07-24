13579e5214c4e45152c8a56121f6d0c8cf4bde3b
daniel <danieldopp@uky.edu>
2017-07-24 16:58:01 -0600

    formatting output change

--- Scratch_Scripts/brat_folder_maker.py
+++ Scratch_Scripts/brat_folder_maker.py
@@ -34,5 +34,5 @@ if __name__ == "__main__":
                    + TASK + "/" + AXIS + "/" + RUN + "/" + file[0] + "/"
         os.makedirs(dir_path, exist_ok=True)
         with open(dir_path + "rank" + "0" * (len(kinase_subset[-1][2]) - len(file[2])) + file[2]
-                  + "_0." + str(floor(float(file[3]))) + "_" + file[1] + ".txt", "w") as outfile:
+                  + "_0." + "".join(file[3].split(".")) + "_" + file[1] + ".txt", "w") as outfile:
             outfile.write(file_text)
