b83667f3e1d28e9fc6b523fdc74f8d8a83381228
daniel <danieldopp@uky.edu>
2017-07-28 14:48:03 -0600

    lol

--- machine_learning_tests/pipelined_full_trainer.py
+++ machine_learning_tests/pipelined_full_trainer.py
@@ -5,13 +5,14 @@ from timeit import default_timer
 import numpy as np
 from random import shuffle
 from itertools import chain
+from math import floor
 
 from sklearn.externals import joblib
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import Normalizer
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from multiprocessing import cpu_count
@@ -30,6 +31,7 @@ if __name__ == "__main__":
         arguments[key] = helpers.replace_pathvar_with_environ(arguments[key])
     # Extract triple of arrays from pickled docs, use doc_id for bag of words, fv_array for doc_prop vector    
     labels, fv_array, doc_ids = pickle.load(open(arguments["feature_vector"], "rb"))
+    print([len(x) for x in (labels, fv_array, doc_ids)])
     # If ini specifies to use less than all documents, take a random sample of the zero terms
     if arguments["training_doc_count"] != "ALL":
         shuffle_size = None
@@ -51,7 +53,7 @@ if __name__ == "__main__":
         else:
             # do nothing if requested size bigger than total zeroes
             shuffle_size = int(arguments["training_doc_count"])
-            if shuffle_size > len(zero_tuples):
+            if shuffle_size >= len(zero_tuples):
                 shuffle_size_too_big = True
         # Cut down zero tuples to random size desired
         if not shuffle_size_too_big:
@@ -61,28 +63,40 @@ if __name__ == "__main__":
             labels = [1 for i in range(len(one_tuples))] + [0 for i in range(len(zero_tuples))]
             fv_array = [item[0] for item in chain(one_tuples, zero_tuples)]
             doc_ids = [item[1] for item in chain(one_tuples, zero_tuples)]
-            print(len(zero_tuples), zero_tuples[:5])
-            print(len(one_tuples), one_tuples[:5])
-            print(labels)
-            print(fv_array[:5])
-            print(doc_ids[:5])
+            # print(len(zero_tuples), zero_tuples[:5])
+            # print(len(one_tuples), one_tuples[:5])
+            # print(labels)
+            # print(fv_array[:5])
+            # print(doc_ids[:5])
     parameters = {}
     pipeline_input = []
     # Select between training method, set parameters and pipeline input for each option
     if arguments["training_method"] == "BOW":
-        features = [arguments["document_path"] + idx + ".txt" for idx in doc_ids]
-        pipeline_input.append(("tfidf_vect", TfidfVectorizer()))
-        parameters.update({
-            "tfidf_vect__input": ["filename"],
-            "tfidf_vect__strip_accents": [None, "unicode"],
-            "tfidf_vect__ngram_range": [(1, 1), (1, 2), (1, 3)],
-            "tfidf_vect__stop_words": [None, "english"],
-            "tfidf_vect__max_df": [x / 10 for x in range(2, 11, 2)],
-            "tfidf_vect__norm": ["l1"],
-            "tfidf_vect__sublinear_tf": [True]
-        })
+        start = default_timer()
+        transf = TfidfVectorizer(
+            input="filename",
+            strip_accents="unicode",
+            ngram_range=(1, 3),
+            stop_words="english",
+            max_df=0.85,
+            norm="l1",
+            sublinear_tf=True
+        )
+        features = transf.fit_transform([arguments["document_path"] + idx + ".txt" for idx in doc_ids])
+        print(str((default_timer() - start) / 60))
+        # pipeline_input.append(("tfidf_vect", TfidfVectorizer()))
+        # parameters.update({
+        #     "tfidf_vect__strip_accents": [None, "unicode"],
+        #     "tfidf_vect__ngram_range": [(1, 1), (1, 2), (1, 3)],
+        #     "tfidf_vect__stop_words": [None, "english"],
+        #     "tfidf_vect__max_df": [x / 10 for x in range(2, 11, 2)],
+        #     "tfidf_vect__norm": ["l1"],
+        #     "tfidf_vect__sublinear_tf": [True],
+        #     "tfidf_vect__min_df": [0.15],
+        # })
     elif arguments["training_method"] == "DOCPROP":
-        features = normalize(np.array(fv_array), norm='l1')
+        transf = Normalizer(norm='l1')
+        features = transf.fit_transform(np.array(fv_array))
         # pipeline_input.append(("pre", Normalizer()))
         # parameters.update({
         #     "pre__norm": ["l1", "l2", "max"]
@@ -95,11 +109,14 @@ if __name__ == "__main__":
         parameters.update({
             "clf__probability": [True],
             "clf__coef0": [0.5],
-            "clf__cache_size": [int(sys.argv[3]) / cpu_count()],
-            "clf__C": [0.01, 0.1, 1.0, 10.0, 100.0],
-            "clf__degree": [1, 2, 3],
+            "clf__cache_size": [5000],
+            "clf__C": [1.0],
+            "clf__degree": [3],
+            "clf__class_weight": ["balanced"],
+            # "clf__C": [0.01, 0.1, 1.0, 10.0, 100.0],
+            # "clf__degree": [1, 2, 3],
             "clf__kernel": ["rbf", "poly"],
-            "clf__class_weight": ["balanced", None],
+            # "clf__class_weight": ["balanced", None],
         })
     elif arguments["classifier"] == "MNNB":
         clf = MultinomialNB()
@@ -116,11 +133,23 @@ if __name__ == "__main__":
             "clf__p": [1, 2, 3, 4],
             "clf__n_jobs": [-1],
         })
+    elif arguments["classifier"] == "LSVM":
+        clf = LinearSVC()
+        parameters.update({
+            "clf__penalty": ["l1", "l2"],
+            "clf__loss": ["hinge", "squared_hinge"],
+            "clf__fit_intercept": [True, False],
+            "clf__C": [0.01, 0.1, 1.0, 10.0, 100.0],
+            # makes dual True if num samples is lequal to num features, false otherwise
+            "clf__dual": [len(features) <= len(features[0])],
+            "clf__class_weight": ["balanced", None],
+        })
     else:
         raise ValueError("unsupported classifier argument given")
+    print(len(features), len(features[0]))
     pipeline_input.append(("clf", clf))
     pipe = Pipeline(pipeline_input)
-    print(pipe, pipeline_input, parameters)
+    print(pipe, pipeline_input, parameters, sep="\n")
     start = default_timer()
     inner_cv = StratifiedKFold(shuffle=True)
     outer_cv = StratifiedKFold(shuffle=True)
@@ -131,7 +160,8 @@ if __name__ == "__main__":
         scoring="roc_auc",
         n_jobs=-1,
         verbose=2,
-        pre_dispatch=cpu_count(),
+        pre_dispatch=floor(cpu_count() * 3/2),
+        error_score=-1
     )
     grid_search.fit(features, labels)
     nested_score = cross_val_score(
@@ -142,10 +172,8 @@ if __name__ == "__main__":
         scoring="roc_auc",
         n_jobs=-1,
     )
-    end = default_timer()
-    print(grid_search)
-    print(grid_search.best_estimator_)
-    print(str((end - start) / 60))
-    print(grid_search.best_score_, nested_score.mean(), grid_search.best_score_ - nested_score.mean())
+    print(grid_search, grid_search.best_estimator_, grid_search.best_estimator_, sep="\n")
+    print(str((default_timer() - start) / 60))
+    print(grid_search.best_score_, nested_score.mean(), grid_search.best_score_ - nested_score.mean(), sep=", ")
     print(grid_search.best_params_)
-    joblib.dump(grid_search, arguments["classifier_path"])
+    joblib.dump((grid_search, transf), arguments["classifier_path"])
--- machine_learning_tests/rank_data.py
+++ machine_learning_tests/rank_data.py
@@ -12,9 +12,7 @@ except ModuleNotFoundError:
 
 def run_ranking(param_tup):
     features = param_tup[1].transform([arguments["doc_source"] + param_tup[0] + ".txt"])
-    if param_tup[2] is not None:
-        features = param_tup[2].transform(features)
-    return param_tup[0], param_tup[3].predict_proba(features)[0][1], param_tup[3].predict(features)
+    return param_tup[0], param_tup[2].predict_proba(features)[0][1], param_tup[2].predict(features)
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
@@ -27,13 +25,12 @@ if __name__ == "__main__":
             helpers.replace_pathvar_with_environ("$STORE/kinase_canonical_to_nxtprot_id.pkl"), "rb"
         )
     )
-    vectorizer, transformer = joblib.load(arguments["vectorizer"])
-    classifier = joblib.load(arguments["classifier"])
+    classifier, transformer = joblib.load(arguments["classifier"])
     with open(arguments["out_path"], "w") as outfile:
         in_dict = pickle.load(open(arguments["possible_matches"], "rb"))
         for kinase, doc_set in in_dict.items():
             with Pool() as p:
-                result = p.map(run_ranking, [(doc, vectorizer, transformer, classifier) for doc in doc_set])
+                result = p.map(run_ranking, [(doc, transformer, classifier) for doc in doc_set])
             result = sorted(result, reverse=True, key=lambda x: x[1])
             count = 0
             for item in result:
