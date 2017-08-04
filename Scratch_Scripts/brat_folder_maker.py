68d81ff72640559aa49226e4b04ea0e95e7c7e03
Danie <danieldopp@uky.edu>
2017-08-04 12:10:43 -0600

    Updated BRAT params

--- Scratch_Scripts/brat_folder_maker.py
+++ Scratch_Scripts/brat_folder_maker.py
@@ -4,11 +4,11 @@ import helper_functions as helpers
 
 if __name__ == "__main__":
     # Change these to work on different axis/tasks
-    AXIS = "BP"
+    AXIS = "DIS"
     TASK = "Task2"
-    RUN = "Run14"
+    RUN = "Run22"
     FILEDIR = "/mnt/lustrefs/store/ddopp/CM_input/FullText/FullTexts_All/"
-    MLRANKSPATH = '/mnt/lustrefs/store/ddopp/ML_Ranking_Output/run14_BP_fulltexts.txt'
+    MLRANKSPATH = '/mnt/lustrefs/store/ddopp/ML_Ranking_Output/run22_DIS_fulltexts.txt'
 
     with open(helpers.replace_pathvar_with_environ("$STORE/kinase_canonical_to_nxtprot_id.pkl"), "rb") as f:
         canon_to_nxt = pickle.load(f)
